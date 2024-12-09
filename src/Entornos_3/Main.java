ff1fa40923f68767649d67e8f0aa51577f66448f
SteveSalazar <eugenio.salazar@live.u-tad.com>
2024-12-09 13:37:37 +0100

    Segundo commit

--- src/Entornos_3/Main.java
+++ src/Entornos_3/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Prueba de eGit");
 		System.out.println("Esto tambien iba en el commit");
+		System.out.println("Esto es del segundo commit");
 
 	}
 
